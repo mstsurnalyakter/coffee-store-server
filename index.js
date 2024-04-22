87878440907f3b9661445e63e1928cd7f4040123
mstsurnalyakter <surnalyms@gmail.com>
2024-04-22 07:03:51 +0600

    delete operation added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,11 +35,25 @@ async function run() {
 
     const coffeeCollection =  client.db("coffeeDB").collection("coffee");
 
+    app.get("/coffee", async (req,res)=>{
+         const cursor = await coffeeCollection.find();
+         const result = await cursor.toArray()
+         res.send(result)
+    })
+
     app.post("/coffee", async (req,res)=>{
         const newCoffee = req.body;
         const result = await coffeeCollection.insertOne(newCoffee);
         console.log(newCoffee);
         res.send(result)
+    })
+
+
+    app.delete("/coffee/:id", async (req,res)=>{
+      const id = req.params.id;
+       const query = { _id: new ObjectId(id)};
+       const result = await coffeeCollection.deleteOne(query);
+       res.send(result);
     })
 
 
